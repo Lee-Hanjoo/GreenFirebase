b700c7a1c2267ef1cc9dd09bbd64f8cfeaac66e9
이한주 <gkswnehcl@gmail.com>
2024-10-18 17:30:46 +0900

    push

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
+import { getMessaging, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBta55VKM4OgBEwxdWGB-suN-3ONoozRck",
@@ -9,10 +10,21 @@ const firebaseConfig = {
   storageBucket: "test-94b65.appspot.com",
   messagingSenderId: "456650788979",
   appId: "1:456650788979:web:df4b5cfa7a6d2114ea1a06",
-  measurementId: "G-BEH6BCHBVB"
+  measurementId: "G-BEH6BCHBVB",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app)
 export const db = getFirestore(app);
+
+let messaging;
+if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
+  messaging = getMessaging();
+}
+
+export const getClientToken = async () => {
+  const vapKey = 'BGgbza_fXIUlBnDsEeeLUMDE0t87vFCx3JuHjku88itq5VFXxzVnhbdm9iNJio_8l1H9cvS7-VLGh6YwHZCyFWc';
+  const currentToken = await getToken(messaging, { vapidKey: vapKey })
+  return currentToken
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 // import Error from "next/error";
 
+
 import { useEffect, useState } from "react";
 import Error from "./_error";
+import ApiTest from "@/pages/ApiTest";
+import { getClientToken } from "@/lib/firebase";
+import axios from "axios";
 
 
 export default function Home() {
@@ -18,14 +22,51 @@ export default function Home() {
 
   if(error) return <Error statusCode={error}/>
 
+  //fcm 토큰발행
+  let token;
+
+  async function clientToken(){
+    token = await getClientToken();
+    console.log(token);
+
+    // Notification.requestPermission()
+    // .then(permission=>{
+    //   console.log(permission,'사용자권한 상태.....')
+    // })
+
+  }
+
+  function pushTest(){
+    const msg = {
+      data:{
+        title:'FCM TEST',
+        body:'FCM 내용.....',
+        icon:'/icons/icons-192.png',
+        image:'/ck.jpg',
+        click_action:'https://www.naver.com'
+      },
+      token
+    }
+    axios.post('/api/fcm', msg)
+  }
+
+
   return (
     <>
       <div><button onClick={err}>error</button></div>
 
       
-      main page...<br/>
+      main page...hanjoo<br/>
       {test}<br/>
       <img src="img_01.jpg" onClick={()=>setTest(2000)}/>
+
+      
+      <article>
+        <h2> 구독하기 (FCM) </h2>
+        <button onClick={clientToken}> 구독하기 </button>
+
+        <button onClick={pushTest}> 푸시알림 </button>
+      </article>
     </>
   );
 }
